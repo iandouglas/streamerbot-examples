d58a1bce3b3a68aafa6b8aec227347240c97d8b7
ian douglas <ian.douglas@iandouglas.com>
2023-12-19 22:55:40 -0700

    first, second, early crew, documentation and code

---
+++ README.md
@@ -0,0 +1,22 @@
+# Streamer.bot Examples
+
+This is a respository of things I've built or tweaked for [Streamer.bot](Streamer.bot) for myself or other streamers on Twitch/YouTube.
+
+## How to use it
+
+Each folder will have an 'export' that will work in Streamer.bot 0.2.x. I will not be generating exports for Streamer.bot 0.1.x. But each folder will also include any C# code needed to build your own actions, subactions, etc along the way.
+
+## Yes, it's open-source
+
+If you find a way to improve it, please share it back here. I'm not an expert at C# or Streamer.bot, I hack things 'til they work then move on to the next thing.
+
+## Yes, I'm here to help
+
+Probably on my [live stream](https://twitch.tv/iandouglas736) but you can contact me as `iandouglas736` on Twitter, Discord, etc.
+
+Check out my [Discord server](https://tig.fyi/discord) and look for the `#streamerbot-ideas` channel for things I'm planning to build, or for what I HAVE built.
+
+## Warranty, etc.
+
+No warranty. You're on your own. I disclaim all responsibility if you use any of my code.
+
---
+++ first, second, early crew/code.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CPHInline
+{
+	public bool Execute()
+	{
+		// get user and reward ifno
+		string userName = args["userName"].ToString();
+		string redemptionId = args["redemptionId"].ToString();
+		string rewardId = args["rewardId"].ToString();
+		string rewardName = args["rewardName"].ToString();
+		
+		int crewSize = Int32.Parse(args["EarlyCrewSizeLimit"].ToString());
+		if (crewSize < 1) {
+			CPH.SendMessage("Sorry, must set EarlyCrewSizeLimit to a value greater than 1");
+		}
+		
+		// get OBS scene and source names
+		string obsScene = args["EarlyCrewOBSScene"].ToString();
+		string obsFirstSource = args["FirstOBSSource"].ToString();
+		string obsSecondSource = args["SecondOBSSource"].ToString();
+		string obsEarlyCrewSource = args["EarlyCrewOBSSource"].ToString();
+
+		// get prefixes and early crew separator, if any
+		string firstUsernamePrefix = args["FirstUsernamePrefix"].ToString() ?? "";
+		string secondUsernamePrefix = args["SecondUsernamePrefix"].ToString() ?? "";
+		string earlyCrewPrefix = args["EarlyCrewPrefix"].ToString() ?? "";
+		string earlyCrewSeparator = args["EarlyCrewSeparator"].ToString() ?? "\n";
+
+		// get channel point redeem names so we know which channel redeem was called
+		string cprFirst = args["FirstChannelPointRedeemName"].ToString();
+		string cprSecond = args["SecondChannelPointRedeemName"].ToString();
+		string cprEarlyCrew = args["EarlyCrewChannelPointRedeemName"].ToString();
+
+		// fetch global variables
+		String firstRedeem = CPH.GetGlobalVar<string>("736FirstRedeemUsername", true);
+		String secondRedeem = CPH.GetGlobalVar<string>("736SecondRedeemUsername", true);
+		List<String> earlyCrew = new List<String>(CPH.GetGlobalVar<string[]>("736EarlyCrewList", true) ?? new string[0]);
+		string tmp = String.Join(",", earlyCrew);
+
+		string obsSourceTarget = "";
+		string obsOutput = "";
+
+		// FIRST
+		if (rewardName == cprFirst) {
+			// check if user is second or early crew
+			if (userName == secondRedeem) {
+				CPH.SendMessage($"Sorry {userName}, you already claimed Second");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+			if (earlyCrew.Contains(userName) || userName == secondRedeem) {
+				CPH.SendMessage($"Sorry {userName}, you're already part of the early crew");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+			
+			// set global
+			CPH.SetGlobalVar("736FirstRedeemUsername", userName, true);
+			obsOutput = $"{firstUsernamePrefix} {userName}";
+			obsSourceTarget = obsFirstSource.Trim();
+		}
+		
+		// SECOND
+		if (rewardName == cprSecond) {
+			// check if user is first or early crew
+			if (userName == firstRedeem) {
+				CPH.SendMessage($"Sorry {userName}, you already claimed First");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+			if (earlyCrew.Contains(userName)) {
+				CPH.SendMessage($"Sorry {userName}, you're already part of the early crew");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+
+			// set global
+			CPH.SetGlobalVar("736SecondRedeemUsername", userName, true);
+			obsOutput = $"{secondUsernamePrefix} {userName}";
+			obsSourceTarget = obsSecondSource.Trim();
+		}
+		
+		// EARLY CREW
+		if (rewardName == cprEarlyCrew) {
+			// check if user is first or second or early crew
+			if (userName == firstRedeem) {
+				CPH.SendMessage($"Sorry {userName}, you already claimed First");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+			if (userName == secondRedeem) {
+				CPH.SendMessage($"Sorry {userName}, you already claimed Second");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+			if (earlyCrew.Contains(userName)) {
+				CPH.SendMessage($"Sorry {userName}, you're already part of the early crew");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+
+			// if list size >= EarlyCrewSizeLimit
+			//CPH.SendMessage($"{earlyCrew.Count} / {crewSize}");
+			if (earlyCrew.Count >= crewSize) {
+				CPH.SendMessage($"Sorry {userName}, the crew is full!");
+				CPH.TwitchRedemptionCancel(rewardId, redemptionId);
+				return false;
+			}
+
+			// add user to the list
+			earlyCrew.Add(userName);
+			CPH.SetGlobalVar("736EarlyCrewList", earlyCrew.ToArray(), true);
+
+			// build text for OBS
+			if (earlyCrewPrefix != "") {
+				obsOutput = $"{earlyCrewPrefix}\n";
+			}
+			obsOutput = String.Join(earlyCrewSeparator, earlyCrew);
+			
+			obsSourceTarget = obsEarlyCrewSource.Trim();
+		}
+
+		//CPH.SendMessage(obsOutput);
+		
+		CPH.ObsSetGdiText(obsScene, obsSourceTarget, obsOutput);
+		
+		return true;
+	}
+}
